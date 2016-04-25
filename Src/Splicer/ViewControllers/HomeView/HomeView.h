c1091bcb448bdf48ce849b89117bbb57a1f1e20d
Ahmad Abdul-Gawad Mahmoud <AhmadAbdulGawadMahmoud@idisk-pro.local>
2016-04-25 22:59:51 +0200

    Start working on Home page :))

---
+++ Src/Splicer/ViewControllers/HomeView/HomeView.h
@@ -0,0 +1,13 @@
+//
+//  HomeView.h
+//  Splicer
+//
+//  Created by Ahmad Abdul-Gawad Mahmoud on 4/25/16.
+//  Copyright © 2016 Ahmad Abdul-Gawad Mahmoud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeView : UIViewController
+
+@end
